77fc520af20184f7d0481fdd5300ee35253b9673
saelozahra <saelozahra14@gmail.com>
2016-09-11 10:36:00 +0430

    نوشتن کد های پرش

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class playerController : MonoBehaviour {
 
+	Rigidbody rigid;
+
+	// vaghti meghdari public tarif beshe az to khode unity Mishe taghiresh dad
+	//jump = mizane shedate paresh
+
+	public float jump = 10 ;
+
 	// Use this for initialization
 	void Start () {
-	
+		rigid = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+	
+	void OnTriggerEnter(Collider col){
+
+		//meghdare avaliye niroye trigger ra 0 mikone
+		// har amali yek aksolamali dare
+		//vaghti mikhad bere bala ye feshar be pain ham dare...
+		rigid.velocity = Vector3.zero;
+
+		//AddForce barayande niroo ra hesab mikonad
+		// nirooyi ke be samte paiin dashtim miad balasho ham hesab mikone
+		rigid.AddForce (Vector3.up * jump);
+	}
+	
+
+
+
 }
