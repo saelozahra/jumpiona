46e59a7a8680856e918ec541e04b8329b01f98de
saelozahra <saelozahra14@gmail.com>
2016-09-11 11:52:38 +0430

    افزودن کد های تغییر مکان دوربین
    و چسباندن دوربین به پلیر و تنظیم اینکه وقتی کارکتر به زمین چسبید دوربین از زمین پائین تر نره

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	/*
+	Transform = safhe ii ke alan mibin meghdar hasho mirize dakhelesh
+	va be hame chizhaye safhe dastrasi dare
+	 */
+
+	public Transform target;
+
+	void Start () {
+	
+	}
+
+	
+	void Update () {
+	 /*
+	 */
+
+	}
+
+
+	void LateUpdate () {
+		/*
+	  * transform doorbin ast
+	  * 
+	  * transform.position va transform.localposition tafavoteshan dar local va global boodan ast
+	  * position global ast
+	  * dar code zir x va z az transform ke hamoon camera hast gerefte mishe va y az target ke dadimesh be player gerefte mishe
+	 */
+		transform.position = new Vector3(transform.position.x,
+		                                 target.position.y,
+		                                 transform.position.z);
+		
+		/* Mathf.Clamp () = baraye meghdare mahdoode ast
+		 * Mathf.Infinity = bi nahayat
+		 */
+		
+		transform.position = new Vector3(transform.position.x,
+		                                 Mathf.Clamp (target.position.y,0,Mathf.Infinity),
+		                                 transform.position.z);
+	}
+}
--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -8,31 +8,47 @@ public class playerController : MonoBehaviour {
 	// vaghti meghdari public tarif beshe az to khode unity Mishe taghiresh dad
 	//jump = mizane shedate paresh
 
-	public float jump = 10 ;
+	public float jump = 10 ,moveSpeed=50;
 
 	// Use this for initialization
 	void Start () {
 		rigid = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frame = 70 bar dar saniye
 	void Update () {
-	
+
+
+		// Input = hameye voroodi ha dar Inputhastand
+		// dar code zir meghdar harkat be chap va rast ra dar dir gharar midahim
+		float dir = Input.GetAxisRaw("Horizontal");
+
+		/* vaghti player be chap harkat konad meghdar dir manfi va vaghti be rast beravad mosbat mishavad
+		 * dar code zir migooyim be chap va rast harkat kon
+		 */
+		rigid.AddForce (Vector3.right*dir*moveSpeed);
+
 	}
 	
 	void OnTriggerEnter(Collider col){
+		// OnTriggerEnter yani vaghti be triger zarbe khord...
+		// veghti khord zamin mire bala dobare
 
-		//meghdare avaliye niroye trigger ra 0 mikone
-		// har amali yek aksolamali dare
-		//vaghti mikhad bere bala ye feshar be pain ham dare...
-		rigid.velocity = Vector3.zero;
+		// dar sharte zir migooyim vaghti ve zamin barkhord kar ...
+		if (col.name == "ground") {
+			//meghdare avaliye niroye trigger ra 0 mikone
+			// har amali yek aksolamali dare
+			//vaghti mikhad bere bala ye feshar be pain ham dare...
+			rigid.velocity = Vector3.zero;
 
-		//AddForce barayande niroo ra hesab mikonad
-		// nirooyi ke be samte paiin dashtim miad balasho ham hesab mikone
-		rigid.AddForce (Vector3.up * jump);
+			//AddForce barayande niroo ra hesab mikonad
+			// nirooyi ke be samte paiin dashtim miad balasho ham hesab mikone
+			rigid.AddForce (Vector3.up * jump);
+		}
 	}
 	
 
 
 
 }
+
