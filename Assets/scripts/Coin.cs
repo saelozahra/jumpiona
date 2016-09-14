6a7ee3adac8e2c0381d92b19a8e0f4138c4ecca5
saelozahra <saelozahra14@gmail.com>
2016-09-14 16:41:19 +0430

    رفع ایرادات خوردن سکه و صوت ها

--- Assets/scripts/Coin.cs
+++ Assets/scripts/Coin.cs
@@ -5,11 +5,13 @@ public class Coin : MonoBehaviour
 {
 
 	public int value = 100;
-	public GameObject deathParticle;
+	public GameObject deathParticle, coinSound;
+	public AudioClip[] sounds;
+	static int soundIndex = 0;
 	
 	void Start ()
 	{
-		
+		soundIndex = 0;
 	}
 	
 	// Update is called once per frame
@@ -20,8 +22,19 @@ public class Coin : MonoBehaviour
 	
 	public void DeleteCoins ()
 	{
+		// namayeshe effect baad az sekke khordan
 		GameObject g = Instantiate (deathParticle, transform.position, transform.rotation) as GameObject;
 		Destroy (g, g.GetComponent<ParticleSystem> ().duration);
+
+		// pakhshe sout baad az seke khordan
+		GameObject s = Instantiate (coinSound, transform.position, transform.rotation) as GameObject;
+		s.GetComponent<AudioSource> ().clip = sounds [soundIndex];
+		s.GetComponent<AudioSource> ().Play ();
+		soundIndex = (soundIndex + 1) % sounds.Length;
+		Destroy (s, s.GetComponent<AudioSource> ().clip.length);
+		//s.GetComponent<AudioSource> ().clip.length = modat zamane clip ra bar migardanad
+
+		// gameObj ra hazf mikonad
 		Destroy (gameObject);
 	}
 }
